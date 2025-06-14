c04544f34fe13f958d2d709e5f545712d7e7fa5b
Kevin PC2 <kevin.kling@outlook.es>
2025-06-14 12:55:14 -0300

    gestion

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const token = sessionStorage.getItem("accessToken");
 
   //Si hay un token, significa que el usuario está autenticado y mostraremos el botón de gestión
-  toggleVisibilityByAuth("gestionNavButton", token);
+  toggleVisibilityByAuth("navbarGestion", token);
 });
 
 /* Funcion que cambia la visibilidad del elemento que le pasamos por parametro */
